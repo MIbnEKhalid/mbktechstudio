e2f5d3ae71e567acf689e245b20a3cc183f21ee5
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-08-10 21:38:12 +0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -52,156 +52,61 @@ function resetMessageBoxColor() {
 }
 
 //Credit: TechLever (youtube.com/@tech-lever)
-document.getElementById("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  resetMessageBoxColor();
-  document.getElementById("message").textContent = "Submitting..";
-  document.getElementById("message").style.display = "block";
-  document.getElementById("submit-button").disabled = true;
-
-  var currentDate = new Date();
-  var day = String(currentDate.getDate()).padStart(2, "0");
-  var month = String(currentDate.getMonth() + 1).padStart(2, "0");
-  var year = currentDate.getFullYear();
-  var hours = String(currentDate.getHours()).padStart(2, "0");
-  var minutes = String(currentDate.getMinutes()).padStart(2, "0");
-  var seconds = String(currentDate.getSeconds()).padStart(2, "0");
-  var timestamp = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-
-  var countryCode = $("#mobile_code").intlTelInput("getSelectedCountryData").dialCode;
-  var inputNumber = document.querySelector('input[name="Number"]').value;
-  var combinedNumber = "+" + countryCode + inputNumber; // Add the '+' sign here
-  document.querySelector('input[name="Timestamp"]').value = timestamp;
-  document.querySelector('input[name="Number"]').value = combinedNumber;
-
-  $(document).ready(function() {
-      $.getJSON("https://api.ipify.org?format=json", function(data) {
-          if (data && data.ip) {
-              $('input[name="IXPX"]').val(data.ip);
-          } else {
-              console.error("Failed to retrieve IP address.");
-          }
-      }).fail(function() {
-          console.error("Failed to retrieve IP address.");
-      });
-  });
-
-  var formData = new FormData(this);
-  var keyValuePairs = [];
-  for (var pair of formData.entries()) {
-      keyValuePairs.push(pair[0] + "=" + pair[1]);
-  }
-
-  var formDataString = keyValuePairs.join("&");
-
-  fetch("https://script.google.com/macros/s/AKfycbz3Gp9r177UzM0z1olj8TW25hBZqIQ7zlMrAdrVoq-dT8gkuqQMwHe7JcmYFwgvcfd_/exec", {
-      redirect: "follow",
-      method: "POST",
-      body: formDataString,
-      headers: {
-          "Content-Type": "text/plain;charset=utf-8",
-      },
-  }).then(function(response) {
-      if (response) {
-          return response;
-      } else {
-          throw new Error("Failed to submit the form.");
-      }
-  }).then(function(data) {
-      document.getElementById("message").textContent = "Message Submitted Successfully!";
+    document.getElementById("form").addEventListener("submit", function(e) {
+      e.preventDefault();
+      resetMessageBoxColor();
+      document.getElementById("message").textContent = "Submitting..";
       document.getElementById("message").style.display = "block";
-      document.getElementById("message").style.backgroundColor = "green";
-      document.getElementById("message").style.color = "beige";
-      document.getElementById("submit-button").disabled = false;
-      document.getElementById("form").reset();
+      document.getElementById("submit-button").disabled = true;
 
-      setTimeout(function() {
+      var currentDate = new Date();
+      var day = String(currentDate.getDate()).padStart(2, "0");
+      var month = String(currentDate.getMonth() + 1).padStart(2, "0");
+      var year = currentDate.getFullYear();
+      var hours = String(currentDate.getHours()).padStart(2, "0");
+      var minutes = String(currentDate.getMinutes()).padStart(2, "0");
+      var seconds = String(currentDate.getSeconds()).padStart(2, "0");
+      var timestamp = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+      var countryCode = $("#mobile_code").intlTelInput("getSelectedCountryData").dialCode;
+      var inputNumber = document.querySelector('input[name="Number"]').value;
+      var combinedNumber = "+" + countryCode + inputNumber;
+      document.querySelector('input[name="Timestamp"]').value = timestamp;
+      document.querySelector('input[name="Number"]').value = combinedNumber;
+
+      var formData = new FormData(this);
+
+      fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      }).then(function(response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Failed to submit the form.");
+        }
+      }).then(function(data) {
+        document.getElementById("message").textContent = "Message Submitted Successfully!";
+        document.getElementById("message").style.display = "block";
+        document.getElementById("message").style.backgroundColor = "green";
+        document.getElementById("message").style.color = "beige";
+        document.getElementById("submit-button").disabled = false;
+        document.getElementById("form").reset();
+
+        setTimeout(function() {
           document.getElementById("message").textContent = "";
           document.getElementById("message").style.display = "none";
           var numberField = document.querySelector(".phoneField");
           numberField.style.display = "none";
           var supportField = document.querySelector(".supportfield");
           supportField.style.display = "none";
-      }, 2000);
-  }).catch(function(error) {
-      console.error(error);
-      document.getElementById("message").textContent = "An error occurred while submitting the form.";
-      document.getElementById("message").style.display = "block";
-  });
-});
-
-// Function to handle change event of the subject select
-
-document.getElementById("subjectSelect").addEventListener("change", function() {
-  var numberField = document.querySelector(".phoneField");
-  var ratingField = document.querySelector(".ratingWeb");
-  var supportField = document.querySelector(".supportfield");
-
-  // Get the value of the selected option
-  var selectedOption = this.value;
-
-  if (selectedOption === "Collaboration") {
-      // Show phone number field for collaboration
-      numberField.style.display = "block";
-      document.querySelector('input[name="Number"]').setAttribute("required", "required");
-
-      // Hide rating field
-      ratingField.style.display = "none";
-      document.querySelectorAll('input[name="stars"]').forEach(function(input) {
-          input.removeAttribute("required");
+        }, 2000);
+      }).catch(function(error) {
+        console.error(error);
+        document.getElementById("message").textContent = "An error occurred while submitting the form.";
+        document.getElementById("message").style.display = "block";
       });
-
-      // Hide support field
-      supportField.style.display = "none";
-      document.querySelectorAll('select[name="support"]').forEach(function(input) {
-          input.removeAttribute("required");
-      });
-  } else if (selectedOption === "Feedback") {
-      // Show rating field for feedback
-      ratingField.style.display = "block";
-      document.querySelectorAll('input[name="stars"]').forEach(function(input) {
-          input.setAttribute("required", "required");
-      });
-
-      // Hide phone number field
-      numberField.style.display = "none";
-      document.querySelector('input[name="Number"]').removeAttribute("required");
-
-      // Hide support field
-      supportField.style.display = "none";
-      document.querySelectorAll('select[name="support"]').forEach(function(input) {
-          input.removeAttribute("required");
-      });
-  } else if (selectedOption === "Support") {
-      // Show support field for support
-      supportField.style.display = "block";
-      document.querySelectorAll('select[name="support"]').forEach(function(input) {
-          input.setAttribute("required", "required");
-      });
-
-      // Show phone number field for support
-      numberField.style.display = "block";
-      document.querySelector('input[name="Number"]').setAttribute("required", "required");
-
-      // Hide rating field
-      ratingField.style.display = "none";
-      document.querySelectorAll('input[name="stars"]').forEach(function(input) {
-          input.removeAttribute("required");
-      });
-  } else {
-      // Hide all additional fields for other options
-      numberField.style.display = "none";
-      document.querySelector('input[name="Number"]').removeAttribute("required");
-      ratingField.style.display = "none";
-      document.querySelectorAll('input[name="stars"]').forEach(function(input) {
-          input.removeAttribute("required");
-      });
-      supportField.style.display = "none";
-      document.querySelectorAll('select[name="support"]').forEach(function(input) {
-          input.removeAttribute("required");
-      });
-  }
-});
+    });
 
 function DownloadCVFile() {
   // Define the file URL
