f8808b7c58fc0336f146e9fa8318db3668af0d6a
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-08-20 16:34:04 +0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -291,8 +291,8 @@ function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toUTCString();
     }
-    const domain = ".mbktechstudio.com";
-    document.cookie = name + "=" + (value || "") + expires + "; path=/" + domain;}
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
 function getCookie(name) {
     var nameEQ = name + "=";
