b3db9102556dcce8cde3cef64b771baf1dd7f81d
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-08-28 22:21:18 +0500

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,6 @@
 // Sticky Navigation Menu JS Code
 let nav = document.querySelector("nav");
- let val;
-window.onscroll = function() {
-	if (document.documentElement.scrollTop > 20) {
-		nav.classList.add("sticky");
- 	} else {
-		nav.classList.remove("sticky");
- 	}
-}
+ let val; 
 
 // Side NavIgation Menu JS Code
 let body = document.querySelector("body");
