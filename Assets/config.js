bb2f642fd7c70692222df8aaa4b7a61a177460d7
Muhammad Bin Khalid <83428194+MIbnEKhalid@users.noreply.github.com>
2024-11-21 15:59:24 +0000

    Update

--- Assets/config.js
+++ Assets/config.js
@@ -1,9 +1,7 @@
-// config.js
 const web3forms_API_KEY = 'd2837145-a5b5-49af-b269-cb79e9b57456';
 
 // Define social media links
 const socialMediaLinks = [
-    { href: "https://signal.me/#eu/0Bxn3qmtp8gtYNuy6eJ_F3WaHWHrQW3uefaC2y01VCWtduFQCwJHvIFL6N4VOtsv", icon: "fa-brands fa-signal-messenger", target: "_blank" },
     { href: "https://instagram.com/mbktech.studios", icon: "fab fa-instagram", target: "_blank" },
     { href: "https://www.facebook.com/people/MBK-Tech-Studio/61559079077988/", icon: "fab fa-facebook-f", target: "_blank" },
     { href: "https://www.linkedin.com/in/muhammad-bin-khalid-89711b25b", icon: "fab fa-linkedin", target: "_blank" },
@@ -11,6 +9,7 @@ const socialMediaLinks = [
     { href: "https://github.com/MIbnEKhalid", icon: "fab fa-github", target: "_blank" },
     { href: "https://youtube.com/@mibnekhalid", icon: "fab fa-youtube", target: "_blank" },
     { href: "https://discord.gg/", icon: "fab fa-discord", target: "_blank" },
+    { href: "https://signal.me/#eu/0Bxn3qmtp8gtYNuy6eJ_F3WaHWHrQW3uefaC2y01VCWtduFQCwJHvIFL6N4VOtsv", icon: "fa-brands fa-signal-messenger", target: "_blank" },
 ];
 
 // Function to populate social media icons
